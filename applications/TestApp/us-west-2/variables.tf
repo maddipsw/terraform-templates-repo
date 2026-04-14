d8043c5851862b7bd7cde5c09aaa6d50f77bd14e
Swetha Bikkasani <swetha.bikkasani@fairview.org>
2026-04-14 17:11:18 +0530

    Modified TestApp files

--- applications/TestApp/us-west-2/variables.tf
+++ applications/TestApp/us-west-2/variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "servers" {
   type = map(object({
     name_prefix              = string
@@ -30,4 +36,82 @@ variable "servers" {
     tags        = map(string)
     volume_tags = map(string)
   }))
+}
+
+variable "key_name" {
+  description = "EC2 key pair name for SSH access"
+  type        = string
+  default     = null
+}
+
+variable "iam_instance_profile" {
+  description = "IAM instance profile to attach to instances"
+  type        = string
+  default     = null
+}
+
+variable "enable_monitoring" {
+  description = "Enable detailed monitoring for EC2 instances"
+  type        = bool
+  default     = false
+}
+
+variable "enable_termination_protection" {
+  description = "Enable termination protection for EC2 instances"
+  type        = bool
+  default     = false
+}
+
+variable "enable_drift_detection" {
+  description = "Enable drift detection for EC2 instances"
+  type        = bool
+  default     = false
+}
+
+variable "metadata_options_http_tokens" {
+  description = "Whether or not the metadata service requires session tokens (IMDSv2)"
+  type        = string
+  default     = "required"
+}
+
+variable "metadata_options_instance_metadata_tags" {
+  description = "Enable instance metadata tags"
+  type        = string
+  default     = "enabled"
+}
+
+variable "metadata_options_http_put_response_hop_limit" {
+  description = "The desired HTTP PUT response hop limit for instance metadata requests"
+  type        = number
+  default     = 1
+}
+
+variable "protect_from_destroy" {
+  description = "Enable lifecycle prevent_destroy for instances"
+  type        = bool
+  default     = false
+}
+
+variable "protect_data_volumes" {
+  description = "Enable lifecycle prevent_destroy for data volumes"
+  type        = bool
+  default     = false
+}
+
+variable "force_detach_volumes" {
+  description = "Force detach volumes on instance termination"
+  type        = bool
+  default     = false
+}
+
+variable "common_tags" {
+  description = "Common tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "common_volume_tags" {
+  description = "Common tags to apply to all volumes"
+  type        = map(string)
+  default     = {}
 }
